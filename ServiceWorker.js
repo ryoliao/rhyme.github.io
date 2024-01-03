47e212e8b53db8d43da92fe40784fffe76b38174
ryo liao <ryo@moregeek.com>
2024-01-04 00:30:15 +0800

    initial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Chaos Galore Unlimited Co.-Rhyme-1.0";
+const contentToCache = [
+    "Build/rhyme.loader.js",
+    "Build/rhyme.framework.js.gz",
+    "Build/rhyme.data.gz",
+    "Build/rhyme.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
